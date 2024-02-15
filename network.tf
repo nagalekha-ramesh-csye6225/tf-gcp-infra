8163f3d2b6bc8b57f16044c4d0f7b861f651b3d5
NagalekhaRamesh <ramesh.na@northeastern.edu>
2024-02-15 14:54:43 -0500

    Creating multiple VPCs

--- network.tf
+++ network.tf
@@ -5,70 +5,39 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc" {
-  name                    = var.vpc_name
-  auto_create_subnetworks = var.auto_create_subnetworks
-  routing_mode = var.routing_mode
-  delete_default_routes_on_create = var.delete_default_routes_on_create
+  count                   = length(var.vpcs)
+  name                    = var.vpcs[count.index].vpc_name
+  auto_create_subnetworks = var.vpcs[count.index].auto_create_subnetworks
+  routing_mode            = var.vpcs[count.index].routing_mode
+  delete_default_routes_on_create = var.vpcs[count.index].delete_default_routes_on_create
 }
 
 resource "google_compute_subnetwork" "webapp" {
-  name          = var.webapp_subnet_name
-  ip_cidr_range = var.webapp_subnet_cidr
-  network       = google_compute_network.vpc.self_link
+  count         = length(var.vpcs)
+  name          = var.vpcs[count.index].webapp_subnet_name
+  ip_cidr_range = var.vpcs[count.index].webapp_subnet_cidr
+  network       = google_compute_network.vpc[count.index].self_link
   region        = var.region
 }
 
 resource "google_compute_subnetwork" "db" {
-  name          = var.db_subnet_name
-  ip_cidr_range = var.db_subnet_cidr
-  network       = google_compute_network.vpc.self_link
+  count         = length(var.vpcs)
+  name          = var.vpcs[count.index].db_subnet_name
+  ip_cidr_range = var.vpcs[count.index].db_subnet_cidr
+  network       = google_compute_network.vpc[count.index].self_link
   region        = var.region
 }
 
 resource "google_compute_route" "webapp_route" {
-  name              = "webapp-route"
-  network           = google_compute_network.vpc.self_link
-  dest_range        = var.dest_range
-  next_hop_gateway   = var.next_hop_gateway
+  count              = length(var.vpcs)
+  name               = "webapp-route-${count.index}"
+  network            = google_compute_network.vpc[count.index].self_link
+  dest_range         = var.vpcs[count.index].dest_range
+  next_hop_gateway   = var.vpcs[count.index].next_hop_gateway
 }
 
 variable "service_account_file_path" {
   description = "Filepath of service-account-key.json"
-  type        = string
-}
-
-variable "dest_range" {
-  description = "Destination IP range for the route"
-  type        = string
-}
-
-variable "next_hop_gateway" {
-  description = "Next hop gateway for the route"
-  type        = string
-}
-
-variable "auto_create_subnetworks" {
-  description = "Whether to auto-create subnetworks in the VPC"
-  type        = bool
-}
-
-variable "routing_mode" {
-  description = "Routing mode for the VPC"
-  type        = string
-}
-
-variable "delete_default_routes_on_create" {
-  description = "Whether to delete default routes on VPC creation"
-  type        = bool
-}
-
-variable "db_subnet_name" {
-  description = "Name of the db subnet to be created"
-  type        = string
-}
-
-variable "db_subnet_cidr" {
-  description = "CIDR range for the db subnet"
   type        = string
 }
 
@@ -82,17 +51,19 @@ variable "region" {
   type        = string
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC to be created"
-  type        = string
-}
-
-variable "webapp_subnet_name" {
-  description = "Name of the webapp subnet to be created"
-  type        = string
-}
-
-variable "webapp_subnet_cidr" {
-  description = "CIDR range for the webapp subnet"
-  type        = string
+variable "vpcs" {
+  description = "List of configurations for multiple VPCs"
+  type        = list(object({
+    vpc_name             = string
+    webapp_subnet_name   = string
+    webapp_subnet_cidr   = string
+    db_subnet_name       = string
+    db_subnet_cidr       = string
+    routing_mode         = string
+    dest_range           = string
+    auto_create_subnetworks      = bool
+    delete_default_routes_on_create = bool
+    next_hop_gateway     = string
+  }))
+  default = []
 }
